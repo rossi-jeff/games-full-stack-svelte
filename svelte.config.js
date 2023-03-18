0a13c9794719cc5e6c19b9a1a56b2c6966d4b15c
Jeff Rossi <rossi.jeff@gmail.com>
2023-03-18 13:35:03 -0400

    static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	}
 };
 
